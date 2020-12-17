c3a98d13678506c3569d40ca0360ada171b7e7bf
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2020-12-17 13:39:52 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 50;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f389835a-f618-47f9-bd78-9a3f21b09723';
+export const MODAL_CLOSE_SEC = 2.5;
